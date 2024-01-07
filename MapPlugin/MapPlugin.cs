bee5c3e87830394c3a977ce51aee7a2e99125e10
cydia-waltz <135396758+cydiawaltz@users.noreply.github.com>
2024-01-07 15:11:26 +0900

    Update MapPlugin.cs

--- MapPlugin/MapPlugin.cs
+++ MapPlugin/MapPlugin.cs
@@ -1,15 +1,11 @@
 ﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.IO.Pipes;
 using System.Text;
 using System.Threading;
-using System.IO.Pipes;
-using AtsEx.PluginHost.Plugins;
 using AtsEx.PluginHost.Native;
+using AtsEx.PluginHost.Plugins;
 using BveTypes.ClassWrappers;
-using System.IO.MemoryMappedFiles;
-using AtsEx.PluginHost.Handles;
-using AtsEx.PluginHost.Input;
-using static System.Collections.Specialized.BitVector32;
-
 
 namespace elementary
 {
@@ -17,9 +13,23 @@ namespace elementary
     [PluginType(PluginType.MapPlugin)]
     internal class MapPluginMain : AssemblyPluginBase
     {
+        private readonly BeaconPassedEventArgs beaconPassedEventArgs;//オブジェクト参照大作
+        private readonly Station station;
         /// プラグインが読み込まれた時に呼ばれる
+        private void Start()
+        {
+            MemoryMappedFile share_mem = MemoryMappedFile.CreateNew("SendToUnity", 1024);
+            MemoryMappedViewAccessor accessor = share_mem.CreateViewAccessor();
+            //残り距離を共有メモリで共有
+            MemoryMappedFile sharemem = MemoryMappedFile.CreateNew("PowerNotch", 1024);
+            MemoryMappedViewAccessor power = sharemem.CreateViewAccessor();
+            //力行ノッチ
+            MemoryMappedFile share = MemoryMappedFile.CreateNew("BrakeNotch", 1024);
+            MemoryMappedViewAccessor brake = share.CreateViewAccessor();
+            //ブレーキノッチ
+        }
         /// 初期化を実装する
-        public MapPluginMain(PluginBuilder builder, VehicleState vehicleState, VehicleSpec vehicleSpec, BeaconPassedEventArgs beaconPassedEventArgs, AtsEx.PluginHost.Handles.HandleSet handleSet, TimeSpan time, AtsEx.PluginHost.Input.KeyBase keyBase, Scenario scenario, TimeManager timeManager, Station station) : base(builder)
+        public MapPluginMain(PluginBuilder builder) : base(builder)
         {
         }
 
@@ -34,15 +44,6 @@ namespace elementary
         public override TickResult Tick(TimeSpan elapsed)
             //TimeSpan elapsed ,PluginBuilder builder, VehicleState vehicleState, VehicleSpec vehicleSpec, BeaconPassedEventArgs beaconPassedEventArgs, AtsEx.PluginHost.Handles.HandleSet handleSet, TimeSpan time, AtsEx.PluginHost.Input.KeyBase keyBase, Scenario scenario, TimeManager timeManager, Station station)
         {
-
-            MemoryMappedFile share_mem = MemoryMappedFile.CreateNew("SendToUnity", 1024);
-            MemoryMappedViewAccessor accessor = share_mem.CreateViewAccessor();
-            //残り距離を共有メモリで共有
-            MemoryMappedFile sharemem = MemoryMappedFile.CreateNew("PowerNotch", 1024);
-            MemoryMappedViewAccessor power = sharemem.CreateViewAccessor();
-            //力行ノッチ
-            MemoryMappedFile share = MemoryMappedFile.CreateNew("BrakeNotch", 1024);
-            MemoryMappedViewAccessor brake = share.CreateViewAccessor();
             //ブレーキノッチ
             //許容範囲
 
@@ -65,13 +66,13 @@ namespace elementary
             byte[] mesnow = Encoding.UTF8.GetBytes(SendNow);
             pipeServer.Write(mesnow, 0, mesnow.Length);
             //次駅到着時刻をUnityへ送る
-            TimeSpan arrival = StationList.Station.ArrivalTime;//停車時に呼び出す
-            TimeSpan past = Station.DepertureTime;//通過時に呼び出す
+            TimeSpan arrival = station.ArrivalTime;//停車時に呼び出す
+            TimeSpan past = station.DepertureTime;//通過時に呼び出す
             string SendArrival = arrival.ToString();//string停車
             string sendPast = arrival.ToString();//string通過
                                                  //内部演算用にミリ秒でもカウント
-            int millinow = Scenario.TimeManager.TimeMilliseconds;//ミリ秒の現在時刻
-            int milliarrival = Station.ArrivalTimeMilliseconds;//ミリ秒の到着時刻
+            int millinow = BveHacker.Scenario.TimeManager.TimeMilliseconds;//ミリ秒の現在時刻
+            int milliarrival = station.ArrivalTimeMilliseconds;//ミリ秒の到着時刻
             int milliDeperture = station.DepertureTimeMilliseconds;//ミリ秒の通過時刻
             //停車時刻・通過時刻を送信
             bool Pass = station.Pass;//停車・通過の判
@@ -101,7 +102,8 @@ namespace elementary
             //新処理（メモリ共有）
 
             //地上子をatc信号に代入
-            switch (BeaconPassedEventArgs.Type)
+            int BeaconType = beaconPassedEventArgs.Type;
+            switch (BeaconType)
             {
                 //ATC信号0
                 case 10:
@@ -170,7 +172,7 @@ namespace elementary
             if (life == 0)
             {
                 //非常ブレーキで減速
-                BrakeNotch = AtsEx.PluginHost.HandleSet.Brake.EmergencyBrakeNotch;
+                BrakeNotch = Native.Handles.Brake.EmergencyBrakeNotch;
                 //Unity側では持ち時間が0になったら音を鳴らしたりする（本家参照）
                 if (speed == 0)
                 {
@@ -185,50 +187,47 @@ namespace elementary
 
             //以下ひたすらインデックスと対応させるただのデスゲーム
             //自列車位置
-            double Location = vehicleState.Location;//ここを残り距離にしませう
-            //ここからテンプラ
-            double H23 = scenario.Route.Stations[0].Location;//北千住停留場
-            double H22 = scenario.Route.Stations[1].Location;//北千住
-            double H21 = scenario.Route.Stations[2].Location;//南千住
-            double H20 = scenario.Route.Stations[3].Location;//三ノ輪
-            double H19 = scenario.Route.Stations[4].Location;//入谷
-            double H18 = scenario.Route.Stations[5].Location;//上野
-            double H17 = scenario.Route.Stations[6].Location;//仲御徒町
-            double H16 = scenario.Route.Stations[7].Location;//秋葉原
-            double H15 = scenario.Route.Stations[8].Location;//小伝馬町
-            double H14 = scenario.Route.Stations[9].Location;//人形町
-            double H13 = scenario.Route.Stations[10].Location;//茅場町
-            double H12 = scenario.Route.Stations[11].Location;//八丁堀
-            double H11 = scenario.Route.Stations[12].Location;//築地
-            double H10 = scenario.Route.Stations[13].Location;//東銀座
-            double H09 = scenario.Route.Stations[14].Location;//銀座
-            double H08 = scenario.Route.Stations[15].Location;//日比谷
-            double H07 = scenario.Route.Stations[16].Location;//霞が関
-            double H06 = scenario.Route.Stations[17].Location;//虎ノ門ヒルズ
-            double H05 = scenario.Route.Stations[18].Location;//神谷町
-            double H04 = scenario.Route.Stations[19].Location;//六本木
-            double H03 = scenario.Route.Stations[20].Location;//広尾
-            double H02 = scenario.Route.Stations[21].Location;//恵比寿
-            double H01 = scenario.Route.Stations[22].Location;//中目黒
-            double H00 = scenario.Route.Stations[23].Location;//中目黒留置線（ここまで実装できるかは不明）
-            accessor.Write(0, Location);//現在位置を送信
-
+            double Location = Native.VehicleState.Location;//ここを残り距離にしませう
+            //ここから過去の努力の遺物
+            //double H23 = BveHacker.Scenario.Route.Stations[0].Location;//北千住停留場
+            //double H22 = BveHacker.Scenario.Route.Stations[1].Location;//北千住
+            //double H21 = BveHacker.Scenario.Route.Stations[2].Location;//南千住
+            //double H20 = BveHacker.Scenario.Route.Stations[3].Location;//三ノ輪
+            //double H19 = BveHacker.Scenario.Route.Stations[4].Location;//入谷
+            //double H18 = BveHacker.Scenario.Route.Stations[5].Location;//上野
+            //double H17 = BveHacker.Scenario.Route.Stations[6].Location;//仲御徒町
+            //double H16 = BveHacker.Scenario.Route.Stations[7].Location;//秋葉原
+            //double H15 = BveHacker.Scenario.Route.Stations[8].Location;//小伝馬町
+            //double H14 = BveHacker.Scenario.Route.Stations[9].Location;//人形町
+            //double H13 = BveHacker.Scenario.Route.Stations[10].Location;//茅場町
+            //double H12 = BveHacker.Scenario.Route.Stations[11].Location;//八丁堀
+            //double H11 = BveHacker.Scenario.Route.Stations[12].Location;//築地
+            //double H10 = BveHacker.Scenario.Route.Stations[13].Location;//東銀座
+            //double H09 = BveHacker.Scenario.Route.Stations[14].Location;//銀座
+            //double H08 = BveHacker.Scenario.Route.Stations[15].Location;//日比谷
+            //double H07 = BveHacker.Scenario.Route.Stations[16].Location;//霞が関
+            //double H06 = BveHacker.Scenario.Route.Stations[17].Location;//虎ノ門ヒルズ
+            //double H05 = BveHacker.Scenario.Route.Stations[18].Location;//神谷町
+            //double H04 = BveHacker.Scenario.Route.Stations[19].Location;//六本木
+            //double H03 = BveHacker.Scenario.Route.Stations[20].Location;//広尾
+            //double H02 = BveHacker.Scenario.Route.Stations[21].Location;//恵比寿
+            //double H01 = BveHacker.Scenario.Route.Stations[22].Location;//中目黒
+            //double H00 = BveHacker.Scenario.Route.Stations[23].Location;//中目黒留置線（ここまで実装できるかは不明）
+            int index = BveHacker.Scenario.Route.Stations.CurrentIndex;//次駅の駅インデックス
+            double NextLocation =BveHacker.Scenario.Route.Stations[index].Location - BveHacker.Scenario.LocationManager.Location;//
+            accessor.Write(0, NextLocation);//次駅までの距離を送信
+            
             //実装したい機能
             //現在時刻・到着時刻>>OK
             //持ち時間（遅れでFinal式）>>OK
             //持ち時間（ATC作動時Bをいれてないと予告無視） >>OK
             //定通（ピッタなら加点）>>OK
-            //残り距離
+            //残り距離 >>OK
             //定通ポイント（独自地上子？）
             //定速ポイント（同）
             //ノッチ数変更通知 >>OK
 
             return new VehiclePluginTickResult();
-        }
-
-        public override TickResult Tick(TimeSpan elapsed)
-        {
-            throw new NotImplementedException();
         }
     }
 }    
